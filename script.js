a7fc01120984a2d14a47e0df5e2eac932830ec8c
asishcs2011010 <80618127+asishcs2011010@users.noreply.github.com>
2026-04-27 16:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+
+const elem = document.querySelector("#buttons");
+
+elem.addEventListener('click', (e) => {
+  
+    if (!e.target.classList.contains("btn") && !e.target.classList.contains("stop")) 
+        {
+        return;
+        }
+  const btnElem = e.target;
+  const text = btnElem.innerText.trim().toLowerCase();
+
+  const audioTrack = document.querySelector(`#${text}`);
+  
+    document.querySelectorAll(".audio").forEach((audio) => {
+        audio.pause();
+        audio.currentTime = 0;
+    })
+    if (text !== "stop" && audioTrack) {
+      audioTrack.play();
+   }
+})
